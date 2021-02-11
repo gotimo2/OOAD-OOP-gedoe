c9b6f32431ee9232105693dc4aeccda5a3b2f4d9
gotimo2 <61739017+gotimo2@users.noreply.github.com>
2021-02-11 01:39:55 +0100

    P2B

---
+++ P2B/src/com/company/Main.java
@@ -0,0 +1,69 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Voetbalclub ajx = new Voetbalclub("Ajax      ");
+        Voetbalclub feij = new Voetbalclub("Feijenoord");
+
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('g');
+        System.out.println("Feijenoord punten: " + feij.aantalPunten());
+        System.out.println("Ajax gespeeld: " + ajx.aantalGespeeld());
+        System.out.println();
+
+        System.out.println(ajx);
+        System.out.println(feij);
+    }
+}
+
+class Voetbalclub {
+
+    private String naam = "";
+    private int aantalGewonnen = 0;
+    private int aantalGelijk = 0;
+    private int aantalVerloren = 0;
+    private int aantalPunten = 0;
+    private int aantalGespeeld = 0;
+
+    public void verwerkResultaat(char ch) {
+        if (ch == 'w') {
+            aantalGespeeld++;
+            aantalGewonnen++;
+            aantalPunten = aantalPunten + 3;
+            //System.out.println("wedstrijd gewonnen " + aantalPunten);
+        }
+        if (ch == 'g') {
+            aantalPunten++;
+            aantalGelijk++;
+            aantalGespeeld++;
+            //System.out.println("wedstrijd gelijk gespeeld " + aantalPunten);
+        }
+        if (ch == 'v'){
+            aantalVerloren++;
+            aantalGespeeld++;
+        }
+    }
+
+    Voetbalclub(String n){
+        this.naam = n;
+    }
+
+
+    public String aantalGespeeld() {
+        return Integer.toString(aantalGespeeld);
+    }
+
+    @Override
+    public String toString() {
+        return this.naam + " " + this.aantalGespeeld + " " + this.aantalGewonnen + " " + this.aantalGelijk + " " + this.aantalVerloren + " " + this.aantalPunten;
+    }
+
+    public String aantalPunten() {
+        return Integer.toString(aantalPunten);
+    }
+
+}
+
